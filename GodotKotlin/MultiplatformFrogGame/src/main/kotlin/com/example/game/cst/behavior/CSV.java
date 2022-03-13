027a25a436f32fac3fcb2fc59fd3a3f127647226
ianloron00 <ianloron00@gmail.com>
2022-03-13 12:45:18 -0300

    training CSV callback finished.

---
+++ GodotKotlin/MultiplatformFrogGame/src/main/kotlin/com/example/game/cst/behavior/CSV.java
@@ -0,0 +1,93 @@
+package com.example.game.cst.behavior;
+
+import java.io.IOException;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class CSV {
+    private PrintWriter writer;
+    private String localPathToFile;
+    private String fileName;
+    private Boolean append;
+
+    public CSV(
+        String localPathToFile, 
+        String fileName,
+        Boolean appendContentToExistingFile
+    ) {
+            this.localPathToFile = localPathToFile;
+            this.fileName = fileName;
+            this.append = appendContentToExistingFile;
+            initializeCallback();
+    }
+
+    public void recordNewEpisode(
+        Long episode,
+        Domain reward,
+        Double epsilon
+    ) {
+        String line = String.format(
+            Locale.US,
+            "%d, %.2f, %.2f",
+            episode,
+            reward.doubleValue(),
+            epsilon
+        );
+        this.writer.println(line);
+        this.writer.flush();
+    }
+
+    public void recordNewEpisode(
+        Integer episode,
+        Domain reward,
+        Double epsilon
+    ) {
+        String line = String.format(
+            Locale.US,
+            "%d, %.2f, %.2f",
+            episode,
+            reward.doubleValue(),
+            episode
+        );
+        this.writer.println(line);
+        this.writer.flush();
+    }
+
+    public void finishCallback() {
+        try {
+            this.writer.flush();
+            this.writer.close();
+        }
+        catch (Exception e) {
+            System.out.println("Error to finish CSV callback");
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeCallback() {
+        try (
+            FileReader fr = new FileReader(
+                this.localPathToFile + this.fileName);
+            BufferedReader br = new BufferedReader(fr);
+        ) {
+            FileWriter fw = new FileWriter(
+                this.localPathToFile + this.fileName,
+                this.append);
+            BufferedWriter bw = new BufferedWriter(fw);
+            this.writer = new PrintWriter(bw);
+            if(br.readLine() == null)
+                this.writer.println("episode, reward, epsilon");
+
+            this.writer.flush();
+
+        } catch (IOException e) {
+            System.out.println("Could not initialize CSV callback");
+            e.printStackTrace();
+        }
+    }
+}
